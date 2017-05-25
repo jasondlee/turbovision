80115effff748967fec013278c7e4df6145cdd8c
Jason Lee <jason@steeplesoft.com>
2017-05-25 17:24:12 -0500

    Script file tweaks

---
+++ swig/runme.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=`pwd`:`pwd`/../library/makes
+mvn install && \
+	java -Djava.library.path=$LD_LIBRARY_PATH -cp target/tvsample-1.0-SNAPSHOT.jar runme 2> error.log && \
+	reset ; stty sane
