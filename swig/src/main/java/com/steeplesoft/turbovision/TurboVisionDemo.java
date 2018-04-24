c335d044bd0d05efa557a10145ca2c4ca0e49188
Jason Lee <jason@steeplesoft.com>
2018-04-24 01:32:31 -0500

    Update README Clean up Java demo class

--- swig/src/main/java/com/steeplesoft/turbovision/TurboVisionDemo.java
+++ swig/src/main/java/com/steeplesoft/turbovision/TurboVisionDemo.java
@@ -24,7 +24,7 @@ import static com.steeplesoft.turbovision.internal.tvisionJNI.kbF6_get;
 import java.io.File;
 import java.util.Arrays;
 
-public class TurboVisionDemo {
+public class TurboVisionDemo extends TApplication {
 
     static {
         Arrays.asList(
@@ -43,67 +43,38 @@ public class TurboVisionDemo {
                 });
     }
 
-    private static class MyApp extends TApplication {
+    public TurboVisionDemo() {
+        super();
+    }
 
-        public MyApp() {
-            super();
-//            TProgram.setStatusLine(createStatusLine(new TRect(0, 0, TScreen.getScreenWidth(), TScreen.getScreenHeight())));
-        }
+    @Override
+    public TStatusLine initStatusLine(TRect r) {
+        r.getA().setY(r.getB().getY() - 1);
+        return new TStatusLine(r, new TStatusDef(0, 0xFFFF)
+                .addItem(new TStatusItem("~Alt-X~ Exit", tvisionJNI.kbAltX_get(), tvisionJNI.cmQuit_get()))
+                .addItem(new TStatusItem("~Alt-F3~ Close Dude", tvisionJNI.kbAltF3_get(), tvisionJNI.cmClose_get())));
+    }
 
-        @Override
-        public TStatusLine initStatusLine(TRect r) {
-            return createStatusLine(r);
-//            return null;
-        }
+    @Override
+    public TMenuBar initMenuBar(TRect r) {
+        r.getB().setY(r.getA().getY() + 1);
 
-        @Override
-        public TMenuBar initMenuBar(TRect r) {
-            r.getB().setY(r.getA().getY() + 1);    // set bottom line 1 line below top line
-            
-            TSubMenu sub1 = new TSubMenu("~F~ile", kbAltF_get());
-            sub1.addItem(new TMenuItem("~O~pen", 200, kbF3_get(), hcNoContext_get(), "F3"))
-                    .addItem(new TMenuItem("~N~ew", 201, kbF4_get(), hcNoContext_get(), "F4"))
-                    .addItem(newLine())
-                            .addItem(new TMenuItem("E~x~it", cmQuit_get(), cmQuit_get(), hcNoContext_get(), "Alt-X"));
-            
-            TSubMenu sub2 = new TSubMenu("~W~indow", kbAltW_get());
-            sub2.addItem(new TMenuItem("~N~ext", cmNext_get(), kbF6_get(), hcNoContext_get(), "F6"))
-                    .addItem(new TMenuItem("~Z~oom", cmZoom_get(), kbF5_get(), hcNoContext_get(), "F5"));
-            
-            
-            TMenuBar menuBar = new TMenuBar(r, sub1.addSubMenu(sub2));
-            
-            /*
-            return new TMenuBar(r,
-                    new TSubMenu("~F~ile", kbAltF_get())
-                    + new TMenuItem("~O~pen", 200, kbF3_get(), hcNoContext_get(), "F3")
-//                    + new TMenuItem("~N~ew", cmMyNewWin_get(), kbF4_get(), hcNoContext_get(), "F4")
-                    + newLine()
-                    + new TMenuItem("E~x~it", cmQuit_get(), cmQuit_get(), hcNoContext_get(), "Alt-X")
-                    + new TSubMenu("~W~indow", kbAltW_get())
-                    + new TMenuItem("~N~ext", cmNext_get(), kbF6_get(), hcNoContext_get(), "F6")
-                    + new TMenuItem("~Z~oom", cmZoom_get(), kbF5_get(), hcNoContext_get(), "F5")
-//                    + new TMenuItem("~D~ialog", cmNewDialog_get(), kbF2_get(), hcNoContext_get(), "F2")
-            // new dialog menu added here
-            );
-*/
-            return menuBar;
-        }
+        TSubMenu sub1 = new TSubMenu("~F~ile", kbAltF_get());
+        sub1.addItem(new TMenuItem("~O~pen", 200, kbF3_get(), hcNoContext_get(), "F3"))
+                .addItem(new TMenuItem("~N~ew", 201, kbF4_get(), hcNoContext_get(), "F4"))
+                .addItem(newLine())
+                .addItem(new TMenuItem("E~x~it", cmQuit_get(), cmQuit_get(), hcNoContext_get(), "Alt-X"));
 
-        private TStatusLine createStatusLine(TRect r) {
-            System.err.println("initStatusLine");
-            r.getA().setY(r.getB().getY() - 1);     // move top to 1 line above bottom
-            final TStatusDef statusDef = new TStatusDef(0, 0xFFFF);
-            System.err.println("adding exit");
-            statusDef.addItem(new TStatusItem("~Alt-X~ Exit", tvisionJNI.kbAltX_get(), tvisionJNI.cmQuit_get()));
-            System.err.println("adding close");
-            statusDef.addItem(new TStatusItem("~Alt-F3~ Close Dude", tvisionJNI.kbAltF3_get(), tvisionJNI.cmClose_get()));
-            return new TStatusLine(r, statusDef);
-        }
+        TSubMenu sub2 = new TSubMenu("~W~indow", kbAltW_get());
+        sub2.addItem(new TMenuItem("~N~ext", cmNext_get(), kbF6_get(), hcNoContext_get(), "F6"))
+                .addItem(new TMenuItem("~Z~oom", cmZoom_get(), kbF5_get(), hcNoContext_get(), "F5"));
+
+        TMenuBar menuBar = new TMenuBar(r, sub1.addSubMenu(sub2));
+        return menuBar;
     }
 
     public static void main(String argv[]) {
-        MyApp app = new MyApp();
+        TurboVisionDemo app = new TurboVisionDemo();
         app.run();
     }
 }
